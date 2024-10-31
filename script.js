37290c9f61a16349ed1b12b451b8ecdfcdc5531b
MOHD MAUSHEEN <97398765+mausheen@users.noreply.github.com>
2024-10-31 10:33:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let fname=document.querySelector("#fname");
+
+fname.addEventListener("blur",()=>{
+	let word=fname.value;
+
+//console.log(word);
+
+	let capital=word.toUpperCase();
+	fname.value=capital;
+	//console.log(capital);
+})
